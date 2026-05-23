d809132386aa38e282963a31e7c5255c4194d17a
Srihari Sajeeva Pillai <srihari.saj@gmail.com>
2026-05-23 13:59:52 +0530

    delete: remove test_orbital_parameters.py file

--- test_orbital_parameters.py
+++
@@ -1,7 +0,0 @@
-from src.orbital_parameters import orbital_parameters
-import json
-import dataclasses
-
-if __name__ == '__main__':
-    o = orbital_parameters(verbose=False)
-    print(json.dumps(dataclasses.asdict(o), default=str, indent=2))
